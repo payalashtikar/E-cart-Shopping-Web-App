2291f5bd0a5b30654440ab4df9fcdc3886c74f0f
unknown <payalashtikar@gmail.com>
2024-03-17 22:19:02 +0530

    implemented-stripe-payment-functionality

---
+++ client/src/Components/other/Cancel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cancel = () => {
+    return (
+        <div className='flex flex-col justify-center items-center p-4 w-full '>
+            <h1 className='font-mono font-bold text-2xl text-red-500'>Payment Cancel !</h1>
+        </div>
+    )
+}
+
+export default Cancel
--- client/src/Components/other/Cart.js
+++ client/src/Components/other/Cart.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { MdDeleteForever } from "react-icons/md";
+import { loadStripe } from '@stripe/stripe-js';
 
 const Cart = ({ cart, removeFromCart }) => {
     const [quantity, setQuantity] = useState({});
@@ -20,6 +21,48 @@ const Cart = ({ cart, removeFromCart }) => {
                 ...prevQuantity,
                 [itemId]: (prevQuantity[itemId] || 0) - 1 // Decrement quantity for the item
             }));
+        }
+    };
+
+    const makePayments = async () => {
+        const stripe = await loadStripe("pk_test_51OtbuqSDNsT47lA7HRo07KBgvFlbMobWvGtQukaC6mjRqx3I0If7lqjGumIMZSJuhFbAuWIabmnnRoXvCrwoYIY0005Tc6uYYt");
+
+        try {
+            const lineItems = cart.map(item => ({
+                price_data: {
+                    currency: 'inr',
+                    product_data: {
+                        name: item.title
+                    },
+                    unit_amount: item.price * 100,
+                },
+                quantity: quantity[item.id] || 1, // will use quantity per item
+            }));
+            console.log('lineItems front', lineItems)
+
+            const response = await fetch("http://localhost:8080/create-checkout-session", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ lineItems })
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to create checkout session');
+            }
+
+            const session = await response.json();
+
+            const result = await stripe.redirectToCheckout({
+                sessionId: session.id
+            });
+
+            if (result.error) {
+                console.error(result.error);
+            }
+        } catch (error) {
+            console.error('Error making payment:', error);
         }
     };
     return (
@@ -45,6 +88,7 @@ const Cart = ({ cart, removeFromCart }) => {
                     ))}
                     <div className='mt-4 font-serif font-bold'>Total Price : {calculateTotalPrice()} Rs.</div>
                     <button className='font-bold hover:text-blue-600  m-4 font-mono py-2 px-4 rounded-lg shadow-lg animated-button btn btn1'
+                        onClick={makePayments}
                     >
                         Pay Now
                     </button>
